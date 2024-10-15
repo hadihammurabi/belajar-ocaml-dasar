938bd78e95cbdcf61b39d20d9b1c32768db02c47
hadihammurabi <hadihammurabi@gmail.com>
2024-10-15 23:23:56 +0700

    feat(math): show how to do basic math

---
+++ 2_math/math.ml
@@ -0,0 +1,6 @@
+Printf.printf "add: %d \n" (1 + 2);;
+Printf.printf "sub: %d \n" (1 - 2);;
+Printf.printf "times: %d \n" (1 * 2);;
+Printf.printf "div: %f \n" (1.0 /. 2.0);;
+Printf.printf "mod: %d \n" (1 mod 2);;
+
---
+++ 2_math/math_input.ml
@@ -0,0 +1,3 @@
+let a = read_int()
+let b = read_int()
+let () = Printf.printf "%d+%d=%d\n" a b (a + b);;
