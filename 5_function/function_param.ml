0e6e5632e90bcf3c9fca494ecbd5282bf89a9d33
hadihammurabi <hadihammurabi@gmail.com>
2024-10-16 12:11:43 +0700

    fix(function): optional parameters example

--- 5_function/function_param.ml
+++ 5_function/function_param.ml
@@ -1,8 +1,10 @@
 let area ~r = 3.14 *. r *. r
 let person ~name:n ~age:a =
   Printf.printf "name: %s, age: %d\n" n a
-
+let add ?(y=0) x = x + y
 let _ =
   Printf.printf "area 10 is %f\n" @@ area ~r:10.0;
-  person ~name:"john thor" ~age:21
+  person ~name:"john thor" ~age:21;
+  Printf.printf "add 6 is %d\n" @@ add 6;
+  Printf.printf "add 6 ~y:2 is %d\n" @@ add 6 ~y:2;
 ;;
